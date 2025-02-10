c7c616906cd9679190b1a58320a84f69630d54c3
STEPHANIE CARTALIER <stephanie.cartalier@gmail.com>
2025-02-10 11:20:31 +0100

    configuration route home

--- app/rooter.js
+++ app/rooter.js
@@ -1,5 +1,8 @@
 // Import de l'usine à routers
 import { Router } from "express";
+import { mainController } from "./controllers/MainController";
+
+
 
 // Création d'un router
 export const router = Router();
