14eabdb925d99bdfaeb5ebd9c69ec96ca73a370e
Kadidhatou Sacko <kadidjatou.sacko@laposte.net>
2025-02-13 17:39:28 +0100

    message confirmation contact à l'envoi du formulaire OK

--- app/controllers/contact-controllers.js
+++ app/controllers/contact-controllers.js
@@ -7,9 +7,9 @@ export const contactController = {
         const pseudo = req.body.pseudo; 
         const mail = req.body.mail;    
         const text = req.body.text;     
-
+        console.log(req.body.pseudo)
         // Passer pseudo à la vue de confirmation
-        res.render('contact-sumbit', { pseudo});  
+        res.render('contact-sumbit', { pseudo, text});  
     }
 };
 
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set("views", "./app/views");
 app.use(express.static("./public"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
