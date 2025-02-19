99b99ae517819b8f51dfedf3301f429392c84f4f
STEPHANIE CARTALIER <stephanie.cartalier@gmail.com>
2025-02-20 00:47:17 +0100

    page home - mobile first opérationnelle, manque le css sur la carte quand le menu est déployé + maj dossier image et suppression images importées lors des tests

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,8 +1,16 @@
+import { Recipe } from "../models/index.js";
+
 export const mainController = {
-    renderHomePage(req, res) {
+    async renderHomePage(req, res) {
         const css = "home";
         const title = "Accueil";
         const js = "index"
-        res.render("home", { css, title, js });
+        const recipes = await Recipe.findAll({
+            include: [ 'difficulty', 'category', 'movie' ]
+          })
+          
+        //console.log(recipes);
+        
+        res.render("home", { css, title, js, recipes });
     },
 }
